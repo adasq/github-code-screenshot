e515e1955ed0f49ec8fb0f4bfd9c5176104f52d7
adasq <asnaebaem@gmail.com>
2018-08-20 22:11:27 +0200

    deploy script updated & unused file removed

--- example.py
+++
@@ -1,8 +0,0 @@
-def reply():
-    print("""
-        Thank you for the review!
-
-        * Yep, I have completely forgotten about putting comments into my contribution :(
-        * According to customizable viewport. I need to check, whether it is a case for long source lines...
-        * Correct, highlighted kewords are related to a javascript. Already fixed it, so there are no more 'this' highlighted in a python sources!
-    """)
