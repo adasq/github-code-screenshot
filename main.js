8c9311fc5124c2f6787d7a464ff90db26bc8572d
adasq3 <adasq3@gmail.com>
2020-08-06 23:32:41 +0200

    .

--- main.js
+++ main.js
@@ -17,24 +17,24 @@ app.get('/stats', require('./src/routes/stats'))
 
 app.get('/proxy', (req, res) => {
     // send queue stats including average computation time
-    res.writeHead(200, 'OK', {
-        'Accept-Ranges': 'bytes',
-        'age': 4095,
-        'cache-control': 'max-age=28800',
-        'Content-Disposition': 'inline; filename="aaaaa.webp"',
-        'Content-Length': 51668,
-        'Content-Type': 'image/webp',
-        'Date': 'Thu, 06 Aug 2020 21:08:19 GMT',
-        'Etag': "167d6-5ac3aeb138326",
-        'Last-Modified': 'Thu, 06 Aug 2020 19:58:55 GMT',
-        'status': 200,
-        'vary': 'Accept',
-    })
+    // res.writeHead(200, 'OK', {
+    //     'Accept-Ranges': 'bytes',
+    //     'age': 4095,
+    //     'cache-control': 'max-age=28800',
+    //     'Content-Disposition': 'inline; filename="aaaaa.png"',
+    //     'Content-Length': 51668,
+    //     'Content-Type': 'image/png',
+    //     'Date': 'Thu, 06 Aug 2020 21:08:19 GMT',
+    //     'Etag': "167d6-5ac3aeb138326",
+    //     'Last-Modified': 'Thu, 06 Aug 2020 19:58:55 GMT',
+    //     'status': 200,
+    //     'vary': 'Accept',
+    // })
     request({
         url: req.query.url,
         headers: {
             'Referer': 'https://www.x-kom.pl/',
-            'Accept': 'image/webp,image/apng,image/*,*/*;q=0.8',
+            'Accept': 'image/apng,image/*,*/*;q=0.8',
 
             'accept-language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7',
             'sec-fetch-dest': 'image',
