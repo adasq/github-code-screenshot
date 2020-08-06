ced58e7b9b22986fcc929fda7eec220abfc062e3
adasq3 <adasq3@gmail.com>
2020-08-06 23:40:22 +0200

    ,

--- main.js
+++ main.js
@@ -17,19 +17,21 @@ app.get('/stats', require('./src/routes/stats'))
 
 app.get('/proxy', (req, res) => {
     // send queue stats including average computation time
-    // res.writeHead(200, 'OK', {
-    //     'Accept-Ranges': 'bytes',
-    //     'age': 4095,
-    //     'cache-control': 'max-age=28800',
-    //     'Content-Disposition': 'inline; filename="aaaaa.png"',
-    //     'Content-Length': 51668,
-    //     'Content-Type': 'image/png',
-    //     'Date': 'Thu, 06 Aug 2020 21:08:19 GMT',
-    //     'Etag': "167d6-5ac3aeb138326",
-    //     'Last-Modified': 'Thu, 06 Aug 2020 19:58:55 GMT',
-    //     'status': 200,
-    //     'vary': 'Accept',
-    // })
+    res.writeHead(200, 'OK', {
+        'Accept-Ranges': 'bytes',
+        'age': 4095,
+        'cache-control': 'max-age=28800',
+        // 'Content-Disposition': 'inline; filename="aaaaa.png"',
+        // 'Content-Length': 51668,
+        'set-cookie': 'a=1',
+        'server': '',
+        // 'Content-Type': 'image/png',
+        'Date': 'Thu, 06 Aug 2020 21:08:19 GMT',
+        'Etag': "167d6-5ac3aeb138326",
+        'Last-Modified': 'Thu, 06 Aug 2020 19:58:55 GMT',
+        'status': 200,
+        'vary': 'Accept',
+    })
     request({
         url: req.query.url,
         headers: {
