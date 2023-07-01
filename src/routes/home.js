5f75a88f173a39dbbf08cc74093d5ec398fc0ac2
adasq <adasq@users.noreply.github.com>
2023-07-01 20:15:24 +0200

    Update home.js

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,5 +3,6 @@ const fs = require('fs');
 const homePage = fs.readFileSync('./src/home.html');
 
 module.exports = async (req, res) => {
+    console.log(req);
     return res.type('html').send(homePage);
 }
